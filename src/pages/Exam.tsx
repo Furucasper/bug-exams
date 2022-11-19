efbe9ae649cf60cc229b3411f2831a3479faa415
Furucasper <furuku.102@gmail.com>
2022-11-19 14:27:17 +0700

    test

--- src/pages/Exam.tsx
+++ src/pages/Exam.tsx
@@ -60,10 +60,8 @@ const Exam = () => {
             method: "PUT",
             url: `${BASE_URL}/${auth.googleID}_1_${files[0].name}`,
             data: files[0],
-            withCredentials: true,
             headers: {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Alloe-Credentials": "true",
+                "Host": "localhost:5173",
             }
 
         }).then((res) => {
